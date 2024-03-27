1a1d5568b5825b75beadbf90f4c12524675c1399
heliosalesjr <heliosalesjr@gmail.com>
2024-03-27 17:53:39 -0300

    navbar evolution

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Navbar />
-        <main>{inter.className}{children}</main>
+        <main>{children}</main>
       
       </body>
     </html>
