a3b2e8ea1dff74ec615e5e9ff729e323e81c43f0
heliosalesjr <heliosalesjr@gmail.com>
2024-03-28 12:01:36 -0300

    navbar implementations

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import "../assets/globals.css"
+
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl text-slate-700 font-workbench">Hie</h1>
+    <h1 className="text-3xl text-slate-700">Hie</h1>
     <Link href='properties'>Show me!</Link>
     </>
   );
