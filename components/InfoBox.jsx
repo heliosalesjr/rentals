bbe78828f7b87979600e302d0ff3b2a4d9945b37
Helio Sales Jr <heliosalesjr@gmail.com>
2024-03-31 10:26:41 -0300

    infobox props

---
+++ components/InfoBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const InfoBox = ( {
+    heading,
+    backgroundColor = bg-slate-200,
+    textColor = text-slate-800,
+    buttonInfo,
+    children
+}) => {
+  return (
+    <div>InfoBox</div>
+  )
+}
