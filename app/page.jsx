b3212c917648faf961dd730b62928b3e7248b107
heliosalesjr <heliosalesjr@gmail.com>
2024-04-01 19:53:35 -0300

    footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
 
 import {Hero} from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes'
-
+import Footer from '@/components/Footer';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
-      
+      <Footer />
     </>
   );
 };
