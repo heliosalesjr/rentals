dbcc39178f5027b7aa4d439a11509e626a49545d
heliosalesjr <heliosalesjr@gmail.com>
2024-04-01 20:28:54 -0300

    properties json file & cards

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,26 @@
-'use client'
-
-import Link from "next/link"
+import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard'
 
 const PropertiesPage = () => {
   return (
     <>
-        <h1 className="text-3xl text-slate-700">Properties</h1>
-        <Link href='/'>Take me back home</Link>
+        <section className="px-4 py-6">
+          <div className='container-xl lg:container m-auto px-4 py-6'>
+            {properties.lenght === 0 ? (
+              <p>No properties found!</p>
+            ) : (
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              { properties.map((property) =>
+                ( 
+                  <PropertyCard key={property._id} property={property} />
+                )
+              )}
+            </div>
+            )
+            }
+            
+          </div>
+        </section>
     </>
   )
 }
